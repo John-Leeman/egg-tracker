27691e262841dacf1b157f5a85aa09718c714ca0
John-Leeman <johndleeman@gmail.com>
2023-04-25 15:31:54 -0700

    Calendar

---
+++ index.js
@@ -0,0 +1,64 @@
+const calendar = document.getElementById('calendar');
+const prevMonthBtn = document.getElementById('prevMonth');
+const nextMonthBtn = document.getElementById('nextMonth');
+const currentMonthYear = document.getElementById('currentMonthYear');
+
+let currentDate = new Date();
+
+function getBatchEvents() {
+
+}
+
+function drawCalendar(date) {
+    const month = date.getMonth();
+    const monthString = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+
+    // Clear the calendar
+    calendar.innerHTML = '';
+
+    // Add day names
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    days.forEach(day => {
+        const dayName = document.createElement('div');
+        dayName.textContent = day;
+        calendar.appendChild(dayName);
+    });
+
+    // Get the first day of the month
+    const firstDay = new Date(year, month, 1).getDay();
+
+    // Get the number of days in the month
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    // Add empty days before the first day
+    for (let i = 0; i < firstDay; i++) {
+        const emptyDay = document.createElement('div');
+        calendar.appendChild(emptyDay);
+    }
+
+    // Add days of the month
+    for (let i = 1; i <= daysInMonth; i++) {
+        const day = document.createElement('div');
+        day.textContent = i;
+        day.className = i + monthString
+        calendar.appendChild(day);
+    }
+
+    // populate batch events
+    getBatchEvents()
+
+    currentMonthYear.textContent = `${monthString} ${year}`;
+}
+
+prevMonthBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    drawCalendar(currentDate);
+});
+
+nextMonthBtn.addEventListener('click', () => {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    drawCalendar(currentDate);
+});
+
+drawCalendar(currentDate);
